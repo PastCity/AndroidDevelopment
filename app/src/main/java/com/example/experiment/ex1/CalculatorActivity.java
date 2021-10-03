a6340eaeafeded3865dbc787391048856529dca2
PastCity <90599522+PastCity@users.noreply.github.com>
2021-10-03 15:12:52 +0800

    Add files via upload

---
+++ app/src/main/java/com/example/experiment/ex1/CalculatorActivity.java
@@ -0,0 +1,16 @@
+package com.example.experiment.ex1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.experiment.R;
+
+public class CalculatorActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_calculator);
+    }
+}
